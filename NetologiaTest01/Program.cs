57aa5c71edcfc8685de27f50e8136e4d1582d502
Cryfore (Kirill) <blackhawksk@yandex.ru>
2024-06-08 00:18:26 +0300

    main 0.001
    Test

---
+++ NetologiaTest01/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetologiaTest01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Go!");
+
+            var a = 1;
+            var b = 0;
+            var c = 1;
+
+            if (a & b || a & (a || b || c))
+            {
+                Console.WriteLine("True");
+            }
+            else 
+            {
+                Console.WriteLine("False"); 
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
