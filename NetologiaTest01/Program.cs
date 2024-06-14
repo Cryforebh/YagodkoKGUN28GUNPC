e7ac2a1778657a79e16e364547c46c593cf461de
Cryfore (Kirill) <blackhawksk@yandex.ru>
2024-06-14 19:11:55 +0300

    main 0.1
    Calculator

--- NetologiaTest01/Program.cs
+++ NetologiaTest01/Program.cs
@@ -10,35 +10,90 @@ namespace NetologiaTest01
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Go!");
+            TryFirst();
 
-            var a = 1;
-            var b = 0;
-            var c = 1;
-
-            if (a & b || a & (a || b || c))
+            void TryFirst()
             {
-                Console.WriteLine("True");
+                string s;
+                Console.WriteLine("Enter two numbers in turn.");
+
+                if (!int.TryParse(Console.ReadLine(), out int a))
+                {
+                    Console.WriteLine("Not a number!");
+                    YesNo();
+                    return;
+                }
+
+                if (!int.TryParse(Console.ReadLine(), out int b))
+                {
+                    Console.WriteLine("Not a number!");
+                    YesNo();
+                    return;
+                }
+
+                Console.WriteLine("Enter the character for the operation.");
+
+                switch (s = Console.ReadLine())
+                {
+                    case "+":
+                        Console.WriteLine("Result of {0} + {1} = {2}", a, b, a + b);
+                        break;
+                    case "-":
+                        Console.WriteLine("Result of {0} - {1} = {2}", a, b, a - b);
+                        break;
+                    case "*":
+                        Console.WriteLine("Result of {0} * {1} = {2}", a, b, a * b);
+                        break;
+                    case "/":
+                        Console.WriteLine("Result of {0} / {1} = {2}", a, b, a / b);
+                        break;
+                    case "&":
+                        Console.WriteLine("Result of {0} & {1} = {2}", a, b, a & b);
+                        break;
+                    case "^":
+                        Console.WriteLine("Result of {0} ^ {1} = {2}", a, b, a ^ b);
+                        break;
+                    default:
+                        Console.WriteLine("Wrong sign!");
+                        break;
+                }
+                YesNo();
             }
-            else 
+
+            void YesNo()
             {
-                Console.WriteLine("False"); 
+                Console.WriteLine("Try first? (yes / no)");
+                switch (Console.ReadLine())
+                {
+                    case "yes":
+                        Console.Clear();
+                        TryFirst();
+                        break;
+                    case "y":
+                        Console.Clear();
+                        TryFirst();
+                        break;
+                    case "no":
+                        Exit();
+                        break;
+                    case "n":
+                        Exit();
+                        break;
+                    default:
+                        Console.Clear();
+                        Console.WriteLine("The word \"no\" or \"yes\" was entered incorrectly. Try again.");
+                        YesNo();
+                        break;
+                }
+
             }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+            void Exit()
+            {
+                Console.WriteLine("Goodbay!");
+                return;
+            }
+            
         }
     }
 }
